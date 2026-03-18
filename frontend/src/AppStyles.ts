c15c0e9dbee4889e03f0e696a3dcdce8e53a11f3
Ohad-Levi123 <ohad.levi.phone@gmail.com>
2026-03-18 18:21:14 +0200

    fixed AppStyles when rebasing

--- frontend/src/AppStyles.ts
+++ frontend/src/AppStyles.ts
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
-import { tss } from "tss-react/mui";
-
-export default tss.create(() => ({
-  app: {
-    display: "flex",
-    justifyContent: "flex-start",
-    flexDirection: "column",
-    height: "100vh",
-    width: '100vw',
-    backgroundColor: "#121212",
-    color: "white",
-  },
-}));
-=======
 import { makeStyles } from 'tss-react/mui';
 
 
 const useStyles = makeStyles()(() => ({
     app: {
+         display: "flex",
+        justifyContent: "flex-start",
+        flexDirection: "column",
         height: '100vh',
         width: '100vw',
-        direction: 'rtl'
+        direction: 'rtl',
+        backgroundColor: "#121212",
+        color: "white",
     }
 }));
 
 export default useStyles;
->>>>>>> 87318d8 (Added SideBar Part1 Q3)
